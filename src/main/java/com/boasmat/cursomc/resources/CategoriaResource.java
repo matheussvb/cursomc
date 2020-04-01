8653f60e4f16846db48be9d828a1ce6f998061e5
Matheus <matheus.svb@hotmail.com>
2020-04-01 18:42:33 -0300

    commit categoria

--- src/main/java/com/boasmat/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/boasmat/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.boasmat.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +17,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService catService;
 
-	@RequestMapping(value = "/{id}/teste", method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 
 		Categoria categoria = catService.buscar(id);
