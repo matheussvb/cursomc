3624393e9168d59cdbf47b8ed99f1f9189e75f21
Matheus <matheus.svb@hotmail.com>
2020-03-31 22:07:47 -0300

    testando a primeira classe de dominio - Categoria

---
+++ src/main/java/com/boasmat/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,29 @@
+package com.boasmat.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boasmat.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method = RequestMethod.GET, path = "/teste")
+	public List<Categoria> listar() {
+
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(categoria1);
+		lista.add(categoria2);
+		return lista;
+
+	}
+
+}
---
+++ src/main/java/com/boasmat/cursomc/resources/Teste.java
@@ -0,0 +1,13 @@
+package com.boasmat.cursomc.resources;
+
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
+public class Teste {
+
+	private String nome;
+	private int idade;
+
+}
