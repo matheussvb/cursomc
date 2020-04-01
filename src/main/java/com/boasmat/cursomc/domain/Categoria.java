2da686b0592ea20cf0758aba9d515a9dd6c9a271
Matheus <matheus.svb@hotmail.com>
2020-03-31 22:17:48 -0300

    h2 funcional

--- src/main/java/com/boasmat/cursomc/domain/Categoria.java
+++ src/main/java/com/boasmat/cursomc/domain/Categoria.java
@@ -2,6 +2,12 @@ package com.boasmat.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,10 +17,13 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String nome;
